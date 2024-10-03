99a7f50644fcdd70ab107d8b020f323aa23e6b09
Heloisa-Moraes <helomoraes2009@hotmail.com>
2024-10-03 11:48:15 -0300

    primeiro componente criado.

--- curso-angular/src/app/app.component.ts
+++ curso-angular/src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { FirstComponentComponent } from './components/first-component/first-component.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'],
+  imports: [FirstComponentComponent]
 })
 export class AppComponent {
   title = 'curso-angular';
