878a867468b8facfcb7bbdb48208ce2df033b41a
Heloisa-Moraes <helomoraes2009@hotmail.com>
2024-10-04 11:15:43 -0300

    dados no template e css no angular.

--- curso-angular/src/app/components/first-component/first-component.component.ts
+++ curso-angular/src/app/components/first-component/first-component.component.ts
@@ -8,5 +8,14 @@ import { Component } from '@angular/core';
   styleUrl: './first-component.component.css'
 })
 export class FirstComponentComponent {
+  name: string = "Heloisa";
+  age = 27;
+  job = 'programadora';
+
+  constructor() { }
+
+  ngOnInit(): void {
+
+  }
 
 }
